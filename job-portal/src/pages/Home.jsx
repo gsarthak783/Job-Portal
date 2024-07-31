cc789b5787164095b0108da00fbaac4d09077ff4
Sarthak <gsarthak783@gmail.com>
2024-07-31 16:38:51 +0530

    homepage api changes

--- job-portal/src/pages/Home.jsx
+++ job-portal/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
 
     setIsLoading(true);
     let fetchData = async () => {
-      const res = await axios.get("http://localhost:1234/job-api/active-jobs")
+      const res = await axios.get("https://job-portal-server-tau-one.vercel.app/job-api/active-jobs")
       let result = res.data.payload;
      
       setJobs(result.reverse());

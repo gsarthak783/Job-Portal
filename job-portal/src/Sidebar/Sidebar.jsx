04647b1d4b61c6063649ce29bed715ce5ab50597
Sarthak <gsarthak783@gmail.com>
2024-08-01 01:21:28 +0530

    Sidebar adjusted

--- job-portal/src/Sidebar/Sidebar.jsx
+++ job-portal/src/Sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ import EmploymentType from './EmploymentType'
 const Sidebar = ({ handleChange }) => {
   return (
     <div className=''>
-      <div className='space-y-5 hidden md:block'>
+      {/* <div className='space-y-5 hidden md:block'>
         <div className='flex-cols flex'>
         <h3 className='flex text-lg font-bold mr-6'>Filters</h3>
         <label htmlFor='reset' className="mx-auto text-lg inline-block  hover:underline text-black font-bold  rounded cursor-pointer">
@@ -19,13 +19,13 @@ const Sidebar = ({ handleChange }) => {
         
         <Location handleChange={handleChange}/>
         <Salary handleChange={handleChange} />
-        {/* <JobPostingDate handleChange={handleChange}/> */}
+        <JobPostingDate handleChange={handleChange}/>
         <WorkExperience handleChange={handleChange}/>
         <EmploymentType handleChange={handleChange}/>
-    </div>
+    </div> */}
 
 
-    <div className="flex flex-col gap-4 p-4 bg-white rounded-md">
+    <div className="flex flex-col gap-4 p-4  rounded-md">
   <div className="flex justify-between items-center">
     <h3 className="text-lg font-bold">Filters</h3>
     <label htmlFor='reset' className="text-lg inline-block hover:underline text-black font-bold rounded cursor-pointer">
@@ -33,17 +33,19 @@ const Sidebar = ({ handleChange }) => {
       Reset
     </label>
   </div>
-  <div className="flex flex-col space-y-4">
-    <div className="flex justify-center space-x-4">
+  <div className="space-y-4 md:space-y-0">
+    <div className="flex flex-row justify-between space-x-4 md:flex-col md:space-x-0 md:space-y-4">
       <Location handleChange={handleChange} />
       <WorkExperience handleChange={handleChange} />
     </div>
-    <div className="flex justify-center space-x-4">
+    <div className="flex flex-row justify-between space-x-4 md:flex-col md:space-x-0 md:space-y-4">
       <Salary handleChange={handleChange} />
       <EmploymentType handleChange={handleChange} />
     </div>
   </div>
 </div>
+
+
 
 
 
